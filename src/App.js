ada6f1420bc4a5a286030041d2318f8bf7600704
Greg Lim <limjunqu@gmail.com>
2017-11-13 11:23:18 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import User from './User';
+import UserForm from './UserForm';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+class App extends Component {  
+
+  render() {
+    return (
+      <div>
+          <BrowserRouter>
+          <div>                            
+            <Switch>   
+              <Route path="/edit/:id" component={UserForm} />               
+              <Route path="/add" component={UserForm} />                       
+              <Route exact path="/" component={User} />             
+              <Route path="/*" component={NotFound} />                       
+            </Switch>   
+          </div>  
+        </BrowserRouter>               
+      </div>
+    );
+  }
+}
+
+export default App;
+
+class NotFound extends Component {
+  render(){
+    return <div>Not Found</div>
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBN9WlmRc3SedmC4agM1G-rYqezGR22iZE",
+    authDomain: "crudproject-45834.firebaseapp.com",
+    databaseURL: "https://crudproject-45834.firebaseio.com",
+    projectId: "crudproject-45834",
+    storageBucket: "crudproject-45834.appspot.com",
+    messagingSenderId: "590481645308"
+  };
+
+firebase.initializeApp(config);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
