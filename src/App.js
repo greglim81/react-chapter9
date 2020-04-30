2beb9ff25bd0da97ee1dc6eca245cdab435d7f97
Greg Lim <limjunqu@gmail.com>
2020-04-30 14:27:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import User from './User';
 import UserForm from './UserForm';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {  
 
